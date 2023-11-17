5993ff802e7d7a19966c45f26a37d22d5d5c5263
倪浩天 <22371325@buaa.edu.cn>
2023-11-17 13:38:43 +0800

    v1.0 CO_AutoTester Pro

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python main.py > pylog.txt
 timeout 5s java -jar Mars_CO_v0.4.1.jar code.asm db nc mc CompactLargeText coL1 ig > log.txt
-java -jar Mars_CO_v0.4.1.jar code.asm db mc CompactDataAtZero a dump .text HexText code.txt
+java -jar Mars_CO_v0.4.1.jar code.asm db mc CompactDataAtZero a dump .text HexText code.txt  # code.txt会下载到run.sh所在目录下
 python runISE.py
 python compare.py
--- runISE.py
+++ runISE.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 import os
 
-projectPath = "D:\\CO\\CO_P5\\pipelineCPU"
-xilinxPath = "C:\\Xilinx\\14.7\\ISE_DS\\ISE\\"
+projectPath = "D:\\CO\\CO_P5\\pipelineCPU"      # 你创建的cpu的工程路径
+xilinxPath = "C:\\Xilinx\\14.7\\ISE_DS\\ISE\\"  # 你的下载的ISE的路径
 
 os.chdir(projectPath)
-os.environ['XILINX'] = xilinxPath # 设置环境变量
-os.system(xilinxPath + "bin/nt64/fuse -nodebug -prj mips.prj -o mips.exe tb > mips.log") # 编译
-os.system("mips.exe -nolog -tclbatch mips.tcl > res.txt") # 运行
+os.environ['XILINX'] = xilinxPath  # 设置环境变量
+os.system(xilinxPath + "bin/nt64/fuse -nodebug -prj mips.prj -o mips.exe tb > mips.log")  # 编译
+os.system("mips.exe -nolog -tclbatch mips.tcl > res.txt")  # 运行
