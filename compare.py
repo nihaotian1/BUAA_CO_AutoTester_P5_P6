b22df9101376556ceab5431b4a215fbe2d9f37bb
cxchhh <13601161004@163.com>
2023-11-19 11:27:35 +0800

    update gitign

--- compare.py
+++ compare.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import os
 
-pathIsm = "D:\\CO\\CO_P5\\pipelineCPU\\res.txt"   # 将res.txt前的路径改为你搭建的cpu的工程路径
+pathIsm = "/mnt/d/co/p6/mips/res.txt"   # 将res.txt前的路径改为你搭建的cpu的工程路径
 pathMars = "log.txt"
 
 ise_log = []
@@ -25,16 +25,22 @@ with open(pathIsm, "r") as file:
 
 length = min(len(mars_log), len(ise_log))
 
-flag = 0
-for i in range(length - 1):
-    if ise_log[i] != mars_log[i]:
-        print("We expect %s, but we got %s" % (mars_log[i], ise_log[i]))
-        flag = 1
+def cmp(i):
+    fail=1
+    if ise_log[i] == mars_log[i]:
+        fail=0
+    if mars_log[i][11] == '*' and i>0 and ise_log[i] == mars_log[i-1]:
+        fail=0
+    if ise_log[i][11] == '*' and i<length-1 and ise_log[i] == mars_log[i+1]:
+        fail=0
+    return fail
 
 with open("compare.txt", "w") as file:
     flag = 0
     for i in range(length - 1):
-        if ise_log[i] != mars_log[i]:
+        fail=cmp(i)
+        if fail:
+            print("We expect %s, but we got %s" % (mars_log[i], ise_log[i]))
             file.write("We expect %s, but we got %s\n" % (mars_log[i], ise_log[i]))
             flag = 1
 
--- runISE.py
+++ runISE.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 import os
 
-projectPath = "D:\\CO\\CO_P5\\pipelineCPU"      # 你创建的cpu的工程路径
-xilinxPath = "C:\\Xilinx\\14.7\\ISE_DS\\ISE\\"  # 你的下载的ISE的路径
+projectPath = "/mnt/d/co/p6/mips"      # 你创建的cpu的工程路径
+xilinxPath = "/opt/Xilinx/14.7/ISE_DS/ISE/"  # 你的下载的ISE的路径
 
 os.chdir(projectPath)
 os.environ['XILINX'] = xilinxPath  # 设置环境变量
-os.system(xilinxPath + "bin/nt64/fuse -nodebug -prj mips.prj -o mips.exe tb > mips.log")  # 编译
-os.system("mips.exe -nolog -tclbatch mips.tcl > res.txt")  # 运行
+os.system(xilinxPath + "bin/lin64/fuse -nodebug -prj mips_txt_stx_beh.prj -o mips mips_txt > mips.log")  # 编译
+os.system("./mips -nolog -tclbatch mips.tcl > res.txt")  # 运行
